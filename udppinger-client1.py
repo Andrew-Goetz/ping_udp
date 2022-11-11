14fe2232ca265f32a72d52e29ae1ff3865da66f0
fantonatos <fotis.antonatos7@gmail.com>
2022-11-11 15:42:40 -0500

    Fix

--- udppinger-client1.py
+++ udppinger-client1.py
@@ -1,31 +1,39 @@
 from socket import *
-import select
 import time
 import datetime
 
+start_time = time.perf_counter()
 sock = socket(AF_INET, SOCK_DGRAM)
-sock.setblocking(0)
-counter = 1
+sock.settimeout(1)
+counter = 0
 
+# This program will send a UDP packet to the server every 3 seconds.
+# and wait for a resonse. If no response is received within 3 seconds the packet times out.
 while True:
-    start = time.perf_counter()
     curtime = datetime.datetime.now()
-    message = "Ping " + str(counter) + " " + str(curtime.strftime('%H:%M:%S'))
 
-    print(f"Sending message {message}")
+    # End after two minutes
+    if (time.perf_counter() - start_time) > 120:
+        break
+
     counter += 1
+    message = f"Ping {counter} {curtime.strftime('%H:%M:%S')}"
+    print(f"Sending \"{message}\"")
     
-    sock.sendto(str.encode(message), ("127.0.0.1", 12000))
+    # Start - Send packet
+    start = time.perf_counter()
+    sock.sendto(message.encode('utf-8'), ("127.0.0.1", 12000))
     
-    ready = select.select([sock], [], [], 120)
-    if not ready[0]:
-        print("Request timed out.")
-        time.sleep(3)
-        continue
-    
-    received = sock.recv(1024)
-    end = time.perf_counter()
-    
-    print(f"Recieved message {received.decode()}. RTT={end-start:.5f}s")
+    # Wait for response
+    try:
+        data, server = sock.recvfrom(1024)
+        response = data.decode('utf-8')
 
+        end = time.perf_counter()
+        print(f"Received \"{response}\" in {end - start:0.4f} seconds")
+    except timeout:
+        print("Request timed out.")
     time.sleep(3)
+
+
+print("Done Pinging")
--- udppinger-client2.py
+++ udppinger-client2.py
@@ -4,49 +4,54 @@ import time
 import datetime
 import sys
 
-
+start_time = time.perf_counter()
 sock = socket(AF_INET, SOCK_DGRAM)
-sock.setblocking(0)
-counter = 1
+sock.settimeout(1)
+counter = 0
 
 loss_counter = 0
 
 rttList = []
 
 try:
-    while True:
-        start = time.perf_counter()
-        curtime = datetime.datetime.now()
-        message = "Ping " + str(counter) + " " + str(curtime.strftime('%H:%M:%S'))
+   while True:
+    curtime = datetime.datetime.now()
+
+    # End after two minutes
+    if (time.perf_counter() - start_time) > 12:
+        break
+
+    counter += 1
+    message = f"Ping {counter} {curtime.strftime('%H:%M:%S')}"
+    print(f"Sending \"{message}\"")
     
-        counter += 1
-        print(f"Sending message {message}")
-        
-        sock.sendto(str.encode(message), ("127.0.0.1", 12000))
-        
-        ready = select.select([sock], [], [], 120)
-        if not ready[0]:
-            print("Request timed out.")
-            loss_counter += 1
-            time.sleep(3)
-            continue
-        
-        received = sock.recv(1024)
+    # Start - Send packet
+    start = time.perf_counter()
+    sock.sendto(message.encode('utf-8'), ("127.0.0.1", 12000))
+    
+    # Wait for response
+    try:
+        data, server = sock.recvfrom(1024)
+        response = data.decode('utf-8')
+
         end = time.perf_counter()
-        
-        print(f"Recieved message {received.decode()}. RTT={end-start:.5f}s")
-        rttList.append(end-start)
-    
-        time.sleep(3)
+        print(f"Received \"{response}\" in {end - start:0.4f} seconds")
+        rttList.append(end - start)
+    except timeout:
+        print("Request timed out.")
+        loss_counter += 1
+    time.sleep(3)
 
 except KeyboardInterrupt:
-    minimum = min(rttList) if len(rttList) > 0 else 0
-    maximum = max(rttList) if len(rttList) > 0 else 0
-    total = len(rttList)
+    pass
 
-    loss_rate = (loss_counter/(counter-1)) * 100
+minimum = min(rttList) if len(rttList) > 0 else 0
+maximum = max(rttList) if len(rttList) > 0 else 0
+total = len(rttList)
 
-    avg = sum(rttList)/len(rttList) if len(rttList) > 0 else 0
+loss_rate = (loss_counter/(counter)) * 100
 
-    print(f"rtt min/max/total/loss_rate/avg: {minimum:.5f}s/{maximum:.5f}s/{total}/{loss_rate:.2f}%/{avg:.5f}s")
-    sys.exit()
+avg = sum(rttList)/len(rttList) if len(rttList) > 0 else 0
+
+print(f"rtt min/max/total/loss_rate/avg: {minimum:.4f}s/{maximum:.4f}s/{total}/{loss_rate:.1f}%/{avg:.4f}s")
+sys.exit()
